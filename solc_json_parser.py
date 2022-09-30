a3ea4eb683483af86a9b3d70927013d8192d4e02
YoungHS <hhhh@u.nus.edu>
2022-09-30 10:20:48 +0800

    put in class and add apis

--- solc_json_parser.py
+++ solc_json_parser.py
@@ -2,11 +2,16 @@ import argparse
 import copy
 import solcx
 import json
+import os
+from typing import Dict, Optional, List, Any, Tuple
+from functools import cached_property
 from fields import Field, Function, ContractData, Modifier
 from version_cfg import v_keys
+
 SOLC_JSON_AST_FOLDER = "./solc_json_ast"
 PARSED_JSON = "./parsed_json"
- 
+
+
 def get_version_key(version):
     print(version)
     if int(version[2]) < 8:
@@ -18,11 +23,12 @@ def get_version_key(version):
 def save_solc_ast(out, name):
     with open(f'./{SOLC_JSON_AST_FOLDER}/{name}_solc_ast.json', 'w') as f:
         json.dump(out, f, indent=4)
+
+
 def compile_to_json(source_code_path):
-    
     def _get_version(source_code):
-        return source_code.split("pragma solidity")[1].split(";")[0].strip().replace('^', '').replace('=','')
-    
+        return source_code.split("pragma solidity")[1].split(";")[0].strip().replace('^', '').replace('=', '')
+
     def _get_source_code(source_code_path):
         # gettting source code from file
         with open(source_code_path, "r") as f:
@@ -37,6 +43,7 @@ def compile_to_json(source_code_path):
     solcx.set_solc_version(version)
     out = solcx.compile_source(source_code, output_values=['ast'], solc_version=version)
     return out, version
+
 
 def get_base_contracts(data):
     base_contracts = []
@@ -54,14 +61,14 @@ def process_function(node, version_key):
         if version_key == "v8":
             for param in parameters['parameters']:
                 param_type_str += param['typeDescriptions']['typeString'] + ", "
-        else: #v4, v5, v6, v7
+        else:  # v4, v5, v6, v7
             for param in parameters['children']:
                 param_type_str += param['attributes']['type'] + ", "
-        
+
         param_type_str = param_type_str[:-2]
         signature += param_type_str + ")"
         return signature
-    
+
     def _get_modifiers(node, version_key):
         modifiers = []
         if version_key == "v8":
@@ -76,58 +83,54 @@ def process_function(node, version_key):
                     if child['children'][0]['attributes']['type'] == "modifier ()":
                         modifiers.append(child['children'][0]['attributes']['value'])
         return modifiers
-        
+
     if version_key == "v8":
-        parameters  = node.get('parameters')
+        parameters = node.get('parameters')
         return_type = node.get('returnParameters')
         modifier_nodes = node
-    else: #v4, v5, v6, v7
-        parameters  = node.get('children')[0]
+    else:  # v4, v5, v6, v7
+        parameters = node.get('children')[0]
         return_type = node.get('children')[1]
         if node.get("attributes").get("modifiers") is None:
             modifier_nodes = {"modifiers": node.get('children')[2:-1]}
         else:
             modifier_nodes = {"modifiers": [None]}
         node = node.get("attributes")
-        
-        
-    
+
     visibility = node.get('visibility')
     if node.get('name') is None or node.get('name') == "":
         name = node.get("kind")
     else:
         name = node.get('name')
-    
+
     inherited_from = ""
-    abstract  = not node.get('implemented')
+    abstract = not node.get('implemented')
     modifiers = _get_modifiers(modifier_nodes, version_key)
-    
-    
-    signature        = _get_signature(name, parameters,  version_key)
-    return_signature = _get_signature("",   return_type, version_key)
+
+    signature = _get_signature(name, parameters, version_key)
+    return_signature = _get_signature("", return_type, version_key)
     return Function(inherited_from=inherited_from, abstract=abstract, visibility=visibility,
                     signature=signature, name=name, return_signature=return_signature, modifiers=modifiers)
 
-    
 
 def process_field(node, version_key):
     if version_key == "v8":
-            pass
-    else: #v4, v5, v6, v7
+        pass
+    else:  # v4, v5, v6, v7
         node = node.get("attributes")
     visibility = node.get('visibility')
     name = node.get('name')
     inherited_from = ""
 
     return Field(inherited_from=inherited_from, visibility=visibility, name=name)
-    
+
 
 def process_modifier(node, version_key):
     if version_key == "v8":
         pass
-    else: #v4, v5, v6, v7
+    else:  # v4, v5, v6, v7
         node = node.get("attributes")
-    
+
     visibility = node.get('visibility')
     name = node.get('name')
     return Modifier(visibility=visibility, name=name)
@@ -135,11 +138,11 @@ def process_modifier(node, version_key):
 
 def get_contract_meta_data(node, version_key):
     if version_key == "v8":
-        pass #do nothing
-    else: #v4, v5, v6, v7
+        pass  # do nothing
+    else:  # v4, v5, v6, v7
         node = node.get("attributes")
         node["abstract"] = not node.get('fullyImplemented')
-    
+
     if node.get('contractKind') == None:
         pass
     contract_kind = node.get('contractKind')
@@ -153,20 +156,18 @@ def get_contract_meta_data(node, version_key):
     base_contracts = get_base_contracts(node.get('baseContracts'))
 
     contract_name = node.get('name')
-    
+
     return contract_kind, is_abstract, contract_name, base_contracts
-    
-    
+
 
 def process_contract(node, version_key):
     keys = v_keys[version_key]
-    
+
     contract_meta_data = get_contract_meta_data(node, version_key)
     contract_kind, is_abstract, contract_name, base_contracts = contract_meta_data
-    
-    
+
     functions = []
-    fields    = []
+    fields = []
     modifiers = []
     for node in node.get(keys.children):
         if node[keys.name] == "FunctionDefinition":
@@ -178,12 +179,13 @@ def process_contract(node, version_key):
         else:
             # not implemented for other types
             pass
-    
-    contract_data = ContractData(is_abstract, contract_name, contract_kind, base_contracts, fields, functions, modifiers)
+
+    contract_data = ContractData(is_abstract, contract_name, contract_kind, base_contracts, fields, functions,
+                                 modifiers)
     return contract_data
 
-def parse(solc_json_ast:dict, version, filename:str):
-    
+
+def parse(solc_json_ast: dict, version, filename: str):
     def _add_inherited_function_fields(data_dict: dict):
         for contract_name, contract in data_dict.items():
             if len(contract.base_contracts) != 0:
@@ -197,49 +199,49 @@ def parse(solc_json_ast:dict, version, filename:str):
                         new_function = copy.deepcopy(function)
                         new_function.inherited_from = base_contract_name
                         contract.functions.append(new_function)
-    
-    def _save_info_json(data_dict ,filename):
+
+    def _save_info_json(data_dict, filename):
         with open(f'./{PARSED_JSON}/{filename}.json', 'w') as f:
             json.dump(data_dict, f, default=lambda obj: obj.__dict__, indent=4)
-            
-    
+
     ast = solc_json_ast.get(list(solc_json_ast.keys())[0]).get('ast')
     data_dict = {}
     version_key = get_version_key(version)
     print("version key: ", version_key)
     keys = v_keys[version_key]
-    
+
     if ast[keys.name] != "SourceUnit" or ast[keys.children] is None:
         raise Exception("Invalid AST")
-    
+
     for i, node in enumerate(ast[keys.children]):
         if node[keys.name] == "PragmaDirective":
             continue
         elif node[keys.name] == "ContractDefinition":
             contract = process_contract(node, version_key)
             data_dict[contract.name] = contract
-            
+
     _add_inherited_function_fields(data_dict)
     _save_info_json(data_dict, filename)
 
+
 ################################################################################
 # 1. the following methods need to be implemented, you can take reference from https://verazt.slack.com/files/U0405RJUNLD/F0423LMJF5Z/solidity_ast.py
-# ./solidity_ast.py:62:    def all_contracts(self) -> List[Node]:
-# ./solidity_ast.py:74:    def all_abstract_contracts(self) -> List[Node]:
-# ./solidity_ast.py:81:    def get_version(unit) -> str:
-# ./solidity_ast.py:105:    def base_contracts_names(self) -> List[str]:
-# ./solidity_ast.py:115:    def pruned_contracts(self) -> List[Node]:
-# ./solidity_ast.py:126:    def pruned_contract_names(self) -> List[str]:
-# ./solidity_ast.py:129:    def fields_in_contract(self,
-# ./solidity_ast.py:161:    def contract_by_name(self, contract_name: str) -> Node:
-# ./solidity_ast.py:167:    def fields_in_contract_by_name(self, contract_name: str, name_only: bool = False, field_visibility: Optional[frozenset] = None,  parent_field_visibility: Optional[frozenset] = FIELD_VISIBILITY_NON_PRIVATE):
-# ./solidity_ast.py:172:    def functions_in_contract(self, contract: Node, name_only: bool = False, function_visibility: Optional[frozenset] = None, check_base_contract=True):
-# ./solidity_ast.py:193:    def functions_in_contract_by_name(self, contract_name: str, name_only: bool = False) -> List[Any]:
-# ./solidity_ast.py:198:    def abstract_functions_in_contract_by_name(self, contract_name: str, name_only: bool = False) -> List[Any]:
-# ./solidity_ast.py:211:    def function_by_name(self, contract_name: str, function_name: str):
+# ./solidity_ast.py:62:   done  def all_contracts(self) -> List[Node]:
+# ./solidity_ast.py:74:   done  def all_abstract_contracts(self) -> List[Node]:
+# ./solidity_ast.py:81:   done  def get_version(unit) -> str:
+# ./solidity_ast.py:105:  done  def base_contracts_names(self) -> List[str]:
+# ./solidity_ast.py:115:  done  def pruned_contracts(self) -> List[Node]:
+# ./solidity_ast.py:126:  done  def pruned_contract_names(self) -> List[str]:
+# ./solidity_ast.py:129:  done  def fields_in_contract(self,
+# ./solidity_ast.py:161:  done  def contract_by_name(self, contract_name: str) -> Node:
+# ./solidity_ast.py:167:  done  def fields_in_contract_by_name(self, contract_name: str, name_only: bool = False, field_visibility: Optional[frozenset] = None,  parent_field_visibility: Optional[frozenset] = FIELD_VISIBILITY_NON_PRIVATE):
+# ./solidity_ast.py:172:  done  def functions_in_contract(self, contract: Node, name_only: bool = False, function_visibility: Optional[frozenset] = None, check_base_contract=True):
+# ./solidity_ast.py:193:  done  def functions_in_contract_by_name(self, contract_name: str, name_only: bool = False) -> List[Any]:
+# ./solidity_ast.py:198:  done  def abstract_functions_in_contract_by_name(self, contract_name: str, name_only: bool = False) -> List[Any]:
+# ./solidity_ast.py:211:  done  def function_by_name(self, contract_name: str, function_name: str):
 # ./solidity_ast.py:216:    def get_all_literals(self):
 # ./solidity_ast.py:236:    def get_all_address(self):
-# ./solidity_ast.py:241:    def get_fallback_functions(self, contract_name: str):
+# ./solidity_ast.py:241:  done  def get_fallback_functions(self, contract_name: str):
 # 2. please consider using class if necessary
 # 3. consider adding test cases in `tests/test_parser.py`
 # 4. either define necessary data structure (with data class, type
@@ -248,21 +250,68 @@ def parse(solc_json_ast:dict, version, filename:str):
 #    input and output.
 
 
+class SolidityAST():
+    def __init__(self, contract_source_path: str):
+        self.contract_source_path: str = contract_source_path
+        self._source_code: str   = self._get_source_code()
+        self.version: str        = self._get_version_from_source_code(self._source_code)
+        self.solc_json_ast: Dict = self.compile_sol_to_json_ast()
+
+    def _get_source_code(self) -> str:
+        with open(self.contract_source_path, 'r') as f:
+            source_code = f.read()
+        return source_code
+
+    def _get_version_from_source_code(self, source_code: str):
+        return source_code.split("pragma solidity")[1].split(";")[0].strip().replace('^', '').replace('=', '')
+
+    def get_version(self) -> str:
+        return self.version
+
+    def compile_sol_to_json_ast(self) -> dict:
+        print("downloading compiler, version: ", self.version)
+        solcx.install_solc(version)
+        solcx.set_solc_version(version)
+        return solcx.compile_source(self._source_code, output_values=['ast'], solc_version=version)
+
+    def save_solc_ast_json(self, name: str):
+        with open(f'./{SOLC_JSON_AST_FOLDER}/{name}_solc_ast.json', 'w') as f:
+            json.dump(self.solc_json_ast, f, indent=4)
+
+    def all_contracts(self) -> List[ContractData]:
+        return []
+
+    def all_abstract_contracts(self) -> List[ContractData]:
+        return []
+
+    def base_contracts_names(self, contract: ContractData) -> List[str]:
+        return contract.base_contracts
+
+    def contract_by_name(self, contract_name: str) -> ContractData:
+        return ContractData()
+
+    def fields_in_contract(self, contract: ContractData) -> List[Field]:
+        return contract.fields
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="path to source code", default="./contracts/whole.sol")
     args = parser.parse_args()
     path = args.path
-    
-    filename = path.split("/")[-1].split(".")[0]
+
+    ast = SolidityAST(path)
+
+    # get filename
+    filename: str = os.path.basename(path)
     print("parsing filename: " + filename)
     solc_json_ast, version = compile_to_json(path)
-    
+
     print("saving solc_json_ast to folder: " + SOLC_JSON_AST_FOLDER)
     save_solc_ast(solc_json_ast, filename)
-    
+
     print("parse solc_json_ast to get info")
     parse(solc_json_ast, version, filename)
-    
+
     print("parsing done")
-    
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,8 @@
 import unittest
 
 import solc_json_parser as parser
-
-contracts_root = './contracts'
+from solc_ast_parser import SolidityAST
+contracts_root = '../contracts'
 
 class TestParser(unittest.TestCase):
     def test_get_correct_version(self):
@@ -23,6 +23,12 @@ class TestParser(unittest.TestCase):
         fns = None
 
     # add tests
+    def test_get_version(self):
+        ast = SolidityAST(f'{contracts_root}/buy051.sol')
+        self.assertEqual('0.5.1', ast.get_version(), 'Version should be 0.5.1')
+
+        ast = SolidityAST(f'{contracts_root}/inheritance_contracts.sol')
+        self.assertEqual('0.7.2', ast.get_version(), 'Version should be 0.7.2')
         
         
 
