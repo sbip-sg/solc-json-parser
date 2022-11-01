7c2f132c0dad4e152386691ec864d98b0b85fe80
YoungHS <hhhh@u.nus.edu>
2022-11-01 20:28:29 +0800

    fix bug with 'raw'

--- solc_json_parser/parser.py
+++ solc_json_parser/parser.py
@@ -212,7 +212,7 @@ class SolidityAst():
         # line number range is the same for all versions
         line_number_range_raw = list(map(int, node.get('src').split(':')))
         line_number_range = get_line_number_range(start_index=line_number_range_raw[0], offset=line_number_range_raw[1], source_code=self.source)
-        start, offset = line_number_range
+        start, offset = line_number_range_raw[0], line_number_range_raw[1]
         raw = self.source[start: start+offset]
 
         if self.version_key == "v8":
