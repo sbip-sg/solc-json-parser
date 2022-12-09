80644b022520f5f1d370233aeb03eac2dbad197e
YoungHS <hhhh@u.nus.edu>
2022-12-09 16:12:16 +0800

    add event dataclass

--- solc_json_parser/fields.py
+++ solc_json_parser/fields.py
@@ -6,7 +6,13 @@ class Field:
     inherited_from:   str
     visibility: str
     name:       str
-    line_num:   tuple # (start, end)
+    line_num:   tuple  # (start, end)
+
+
+@dataclass
+class Modifier:
+    name: str
+    visibility: str
 
 
 @dataclass
@@ -25,27 +31,28 @@ class Function:
 
 
 @dataclass
+class Event:
+    name: str
+    signature:  str
+    anonymous: bool
+    line_num: tuple
+    raw: str
+
+
+@dataclass
 class ContractData:
     abstract:       bool
     name:           str
     kind:           str
     base_contracts: List[int]
-    fields:         list
-    functions:      list
-    modifiers:      list
-    line_num:       tuple # (start, end)
-    contract_id: int # unique id in ast per solc compilation
-
-@dataclass
-class Modifier:
-    name: str
-    visibility: str
+    fields:         List[Field]
+    functions:      List[Function]
+    modifiers:      List[Modifier]
+    line_num:       tuple  # (start, end)
+    contract_id:    int    # unique id in ast per solc compilation
+    events:         List[Event]
 
 
 
 
 
-# [Function(signature='',           return_signature='',   name='receive',  modifiers=[], type='receive'),
-#  Function(signature='receive()',  return_signature='()', name='receive',  modifiers=[], type='function'),
-#  Function(signature='fallback()', return_signature='()', name='fallback', modifiers=[], type='function'),
-#  Function(signature='',           return_signature='',   name='fallback', modifiers=[], type='fallback')]
